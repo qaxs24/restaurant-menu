d5e67c2ebf0b7f5a1c121797c754399a5f8ad9e4
Іван Олійник <ivan.oliinyk.23@pnu.edu.ua>
2025-12-12 18:12:23 +0200

    feat: redesign contacts page layout and styles; enhance contact cards with icons, improve responsiveness, and update structure for better user experience

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contacts',
-  imports: [MatCardModule, MatIconModule],
+  imports: [RouterLink, MatIconModule, MatButtonModule],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
